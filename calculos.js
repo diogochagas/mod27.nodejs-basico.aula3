24c981e6314817c61f411783e09659e9d6156064
Diogo Chagas <diogodvd@gmail.com>
2021-12-14 09:38:22 -0300

    adicionando

---
+++ calculos.js
@@ -0,0 +1,10 @@
+function soma(n1, n2) {
+  return n1 + n2
+}
+
+function multiplicacao(n1, n2)
+
+module.exports = {
+  soma,
+  multiplicacao
+}
---
+++ index.js
@@ -0,0 +1,6 @@
+const soma = require('./calculos.js')
+
+
+const resultado = soma(10, 20)
+
+console.log(resultado)
